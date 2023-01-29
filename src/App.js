7e6fe28d2a589d6a1892696a5e722c4fb37ed6bd
mr.rahmeti <mr.mohammad.rahmati.79@gmail.com>
2023-01-29 09:28:03 -0800

    start project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <link rel='stylesheet' href='/fontawesome/css/all.css'></link>
+    <link rel='stylesheet' href='/fontawesome/css/all.css'/>
     <i className='fa-solid fa-home'></i>
      <h1>hello</h1>
     </div>
